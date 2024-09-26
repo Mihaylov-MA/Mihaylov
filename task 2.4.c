839590fe5198de4cf410f5e39217c9927fbb052b
Mihaylov-MA <mihaylovma1971@gmail.com>
2024-09-26 12:36:25 +0300

    Add files via upload

---
+++ task 2.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+ 
+  int a = 10;
+  int b = 5;
+  int sum = a + b;             
+  float average = sum / 2.0;
+  printf("Сумма: %d\n", sum);
+  printf("Средние арифмитическое: %f\n", average);
+ 
+  return 0;
+}
